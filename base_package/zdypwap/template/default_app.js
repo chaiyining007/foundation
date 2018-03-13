c2709c2782f0c019a1768034656f5fa4ceb73bd8
柴逸宁 <chaiyining@wansecheng.com>
2018-03-13 17:26:58 +0800

    增加模块

---
+++ base_package/zdypwap/template/default_app.js
@@ -0,0 +1,27 @@
+module.exports = (base_data) => `/*页面级，组件 */
+<template>
+    <div class="${base_data.name}">
+        
+    </div>
+</template>
+<script>
+// import data from "./src/data";
+import watch from "./src/watch";
+import methods from "./src/methods";
+import computed from "./src/computed";
+
+export default {
+    data: function(){
+        return {};
+    },
+    watch: watch,
+    methods: methods,
+    computed: computed,
+    components: {},
+    mounted: function() {}
+};
+</script>
+<style lang="sass" scoped>
+@import './sass/style.scss';
+</style>
+`;
