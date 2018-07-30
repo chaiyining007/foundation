fc303bc321c23f6f289fd169804c5ecfacbe81f9
柴逸宁 <jolin.chai@togetu.in>
2018-07-30 19:17:32 +0800

    test

--- bin/create.js
+++ bin/create.js
@@ -41,8 +41,8 @@ const _createFilePromise = (file_name, file_template, download_url) => new Promi
   }
   if (download_url) {
     console.log(`开始 下载${download_url}`)
-    download(download_url, `${packages}${package_name}${path.sep}template${path.sep}`).then(() => {
-      console.log(`下载完成：${download_url},${packages}${package_name}${path.sep}template${path.sep}`)
+    download(download_url, `${PRINT}${path.sep}`).then(() => {
+      console.log(`下载完成：${PRINT}${path.sep}`)
       resolve()
     });
   }
