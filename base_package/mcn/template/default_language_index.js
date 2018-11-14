f3b2c14f8c44952cfe654a90263195627330e315
柴逸宁 <jolin.chai@togetu.in>
2018-11-14 10:53:19 +0800

    修改别名引用

--- base_package/mcn/template/default_language_index.js
+++ base_package/mcn/template/default_language_index.js
@@ -1,10 +1,10 @@
 module.exports = (base_data)=>
-`import { merge } from 'public/helper/libs'
+`import { merge } from 'helper/libs'
 import element_languages from '@/i18n/element-ui.js'
 import base_languages from '@/i18n/languages.json'
 import languages from './languages.json'
 
-import locale from "public/libs/locale"
+import locale from "libs/locale"
 
 const messages = merge(element_languages,base_languages,languages)
 
